2102dc3002681adabba1c557c3f9c40ee8d8be45
Godotcoffee <641276343@qq.com>
2023-07-18 14:51:17 +0800

    update

--- src/task/src/task4.cpp
+++ src/task/src/task4.cpp
@@ -27,27 +27,7 @@ namespace task4 {
             mixValue = std::max(0.0f, mixValue - 0.001f);
         }
     }
-
-    const char *vertexShaderSource = 
-        "#version 330 core\n"
-        "layout (location = 0) in vec3 aPos;\n"
-        "layout (location = 1) in vec3 aColor;\n"
-        "\n"
-        "out vec3 ourColor;\n"
-        "void main() {\n"
-        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0f);\n"
-        "   ourColor = aColor;\n"
-        "}\0";
-
-    const char *fragmentShaderSource =
-        "#version 330 core\n"
-        "out vec4 FragColor;\n"
-        "\n"
-        "in vec3 ourColor;\n"
-        "void main() {\n"
-        "   FragColor = vec4(ourColor, 1.0f);\n"
-        "}\0";
-    }
+}
 
 int main4(int argc, char const *argv[])
 {
